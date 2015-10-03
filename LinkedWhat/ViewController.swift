646c31c80cf9cd60b3c9864b2f0bc58b38ce2c25
Tim Broder <timothy.broder@gmail.com>
2015-10-03 10:59:55 -0400

    init

---
+++ LinkedWhat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LinkedWhat
+//
+//  Created by Tim Broder on 10/3/15.
+//  Copyright © 2015 Tim Broder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
